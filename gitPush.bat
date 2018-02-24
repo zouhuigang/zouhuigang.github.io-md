ff86fd29afa0532e1366f2bbef255ef2081071a9
hgzou <hgzou@51tywy.com>
2018-02-24 14:31:13 +0800

    更新内容

--- gitPush.bat
+++ gitPush.bat
@@ -2,3 +2,10 @@
 git add -A
 git commit -am "更新内容"
 git push origin master
+
+#更新public库
+hugo --theme=hugo-travelify-theme --buildDrafts --baseUrl="https://zouhuigang.github.io/"
+cd public
+git add -A
+git commit -am "更新网站内容"
+git push origin master
